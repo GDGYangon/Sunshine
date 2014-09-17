3b3fa0b6898030e367efc50814aaae94efe8ebdf
Ye Lin Aung <me@yelinaung.com>
2014-09-17 22:16:23 +0630

    add internet permission and show output in the logcat

--- app/src/main/java/com/androidbootcamp/sunshine/ForecastFragment.java
+++ app/src/main/java/com/androidbootcamp/sunshine/ForecastFragment.java
@@ -140,6 +140,8 @@ public class ForecastFragment extends Fragment {
                     forecastJsonStr = null;
                 }
                 forecastJsonStr = buffer.toString();
+
+                Log.v(LOG_TAG, "Fetched weather string " + forecastJsonStr);
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attemping
