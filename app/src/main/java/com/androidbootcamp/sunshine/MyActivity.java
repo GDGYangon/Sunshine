67c29926429421a1d1a87d892149cdccad0e1b09
Ye Lin Aung <me@yelinaung.com>
2014-09-17 09:22:15 +0630

    end of first lesson

--- app/src/main/java/com/androidbootcamp/sunshine/MyActivity.java
+++ app/src/main/java/com/androidbootcamp/sunshine/MyActivity.java
@@ -1,9 +1,19 @@
 package com.androidbootcamp.sunshine;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MyActivity extends Activity {
@@ -12,6 +22,11 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 
 
@@ -35,5 +50,52 @@ public class MyActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        private ArrayAdapter<String> mForecastAdapter;
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+
+            String[] forecastArray = {
+
+                    "Today - Sunny - 88/60",
+                    "Tomorrow - Foggy - 70/40",
+                    "Friday - Cloudy - 72/63",
+                    "Saturday - Heavy Rain - 65/56"
+            };
+
+
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+
+            mForecastAdapter = new ArrayAdapter<String>(
+                    // The current context (this fragment's parent activity
+                    getActivity(),
+                    // ID of list item layout
+                    R.layout.list_item_forecast,
+                    // ID of the TextView to populate
+                    R.id.list_item_forecast_textview,
+                    // Forecast Data
+                    weekForecast
+            );
+
+
+            // Get a reference to a ListView and attach the adapter to the ListView
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
+
+            return rootView;
+        }
     }
 }
