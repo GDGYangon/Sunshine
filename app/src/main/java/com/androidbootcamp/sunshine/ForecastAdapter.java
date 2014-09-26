bd6b2755d9548308e5407f4bd4354f32bed00152
poepoe <poepoe@zwenex.com>
2014-09-26 20:28:24 +0630

    Custom Array Adapter Added

--- app/src/main/java/com/androidbootcamp/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/androidbootcamp/sunshine/ForecastAdapter.java
@@ -1,21 +1,46 @@
 package com.androidbootcamp.sunshine;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 /**
  * Created by Ye Lin Aung on 14/09/26.
  */
-public class ForecastAdapter extends ArrayAdapter<String> {
+public class ForecastAdapter extends ArrayAdapter<Item> {
 
-    public ForecastAdapter(Context context, int resource, String[] objects) {
+    Context context;
+
+    public ForecastAdapter(Context context, int resource, List<Item> objects) {
         super(context, resource, objects);
+
+        this.context = context;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+
+
+
+        LayoutInflater inflater =
+                (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(R.layout.list_item_forecast, null, false);
+
+        TextView textView = (TextView)convertView.findViewById(R.id.list_item_forecast_textview);
+        ImageView imageView = (ImageView)convertView.findViewById(R.id.list_item_forecast_image);
+
+        Item item = (Item)getItem(position);
+        textView.setText(item.getText());
+        Picasso.with(context).load(item.getImage()).into(imageView);
+        return convertView;
+
     }
 }
---
+++ app/src/main/java/com/androidbootcamp/sunshine/Item.java
@@ -0,0 +1,26 @@
+package com.androidbootcamp.sunshine;
+
+/**
+ * Created by poepoe on 9/26/14.
+ */
+public class Item {
+    String text;
+    String image;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
+
